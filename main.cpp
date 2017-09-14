4c59817e9c228f3a0c2a6985eda1a5b9e81b87b9
Dario Dematties <dariodematties@yahoo.com.ar>
2017-09-14 14:58:59 -0300

    Incorporate modifications in order to test computation.

--- main.cpp
+++ main.cpp
@@ -6,19 +6,34 @@
 
 int main(int argc, char* argv[])
 {
-	// Check the number of parameters
-	if (argc != 3) {
+	if (argc == 3) {
+		std::size_t	vector_length = std::atoi(argv[1]);
+		std::size_t	number_of_iterations = std::atoi(argv[2]);
+		/* initialize random seed: */
+		std::srand(std::time(NULL));
+		
+		Average	averageobj(vector_length,number_of_iterations);
+		auto	average = averageobj.computeAverage();
+		std::cout << "Average is: " << average << "\n";
+	}
+	else if (argc == 4) {
+		std::size_t	vector_length = std::atoi(argv[1]);
+		std::size_t	number_of_iterations = std::atoi(argv[2]);
+		double		test_value = std::atof(argv[3]);
+		/* initialize random seed: */
+		std::srand(std::time(NULL));
+		std::vector<double>	vec(vector_length,test_value);
+		
+		Average	averageobj(vector_length,number_of_iterations,vec);
+		auto	average = averageobj.computeAverage();
+		std::cout << "Average is: " << average << "\n";
+	}
+	else {
 		// Tell the user how to run the program
 		std::cerr << "Usage: " << argv[0] << " vector_length number_of_iterations" << std::endl;
+		std::cerr << "Or usage: " << argv[0] << " vector_length number_of_iterations test_value" << std::endl;
+		std::cerr << "vector_length: test memory capacity \nnumber_of_iterations: test the parallelization capacity" << std::endl;
+		std::cerr << "and optional test_value: test the correct computation" << std::endl;
 		return 1;
 	}
-
-	std::size_t	vector_length = std::atoi(argv[1]);
-	std::size_t	number_of_iterations = std::atoi(argv[2]);
-	/* initialize random seed: */
-	std::srand(std::time(NULL));
-	
-	Average	averageobj(vector_length,number_of_iterations);
-	auto	average = averageobj.computeSum();
-	std::cout << "Average is: " << average << "\n";
 }
